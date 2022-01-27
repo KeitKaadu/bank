1a7f2030be1ebbd5793a32b49c87c6a7144370c2
opilane <keitkaadu@yahoo.co.uk>
2022-01-27 15:37:30 +0200

    TASK-3 updateOwnerDetails

--- src/main/java/ee/bcs/bank/restbank/BankController.java
+++ src/main/java/ee/bcs/bank/restbank/BankController.java
@@ -63,6 +63,11 @@ public class BankController {
     public RequestResult receiveNewTransaction(@RequestBody TransactionDto transactionDto) {
         return transactionService.receiveNewTransaction(bank, transactionDto);
     }
+    @PutMapping("/update/owner")
+    public RequestResult updateOwnerDetails(@RequestBody AccountDto accountDto) {
+
+        return accountService.updateOwnerDetails(bank.getAccounts(), accountDto);
+    }
 
 
 
